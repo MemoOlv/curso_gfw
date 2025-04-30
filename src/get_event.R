2121d1a1bfcc0d716ac6ca69cb66b6dd0a78c41f
MemoOlv <guillermo.olvera@islas.org.mx>
2025-04-30 11:55:40 -0600

    ✨ Add example for `get_event()`

---
+++ src/get_event.R
@@ -0,0 +1,28 @@
+info_vessel <- gfwr::get_vessel_info(
+    query = 431782000,
+    search_type = "search",
+    key = gfwr::gfw_auth())
+
+id_test <- info_vessel$selfReportedInfo$vesselId[4]
+
+visitas_puerto <- gfwr::get_event(
+    event_type = "PORT_VISIT",
+    vessels = id_test,
+    start_date = "2017-01-26",
+    end_date = "2022-12-31",
+    key = gfwr::gfw_auth()
+)
+
+unnested_visitas_puerto <- visitas_puerto |> 
+    tidyr::unnest_wider(event_info)
+
+print(unnested_visitas_puerto)
+
+visitas_puerto <- gfwr::get_event(
+    event_type = "PORT_VISIT",
+    vessels = id_test,
+    start_date = "2017-01-26",
+    end_date = "2022-12-31",
+    confidence = c(4),
+    key = gfwr::gfw_auth()
+)
